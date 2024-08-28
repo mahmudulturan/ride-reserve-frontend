78a4258eed140ef4d3fab539404723b77550dc55
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-08-29 01:15:12 +0600

    setup: tailwind css

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 const MainLayout: FC = () => {
     return (
         <div>
-            <div>Navbar</div>
+            <div className='text-3xl'>Navbar</div>
             <Outlet />
             <div>Footer</div>
         </div>
