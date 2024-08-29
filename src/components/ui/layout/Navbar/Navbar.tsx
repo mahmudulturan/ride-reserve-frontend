cba361cb945a6988e95adcfb25bcbfd5d65ca76d
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-08-29 10:25:11 +0600

    fix: navbar color issues

--- src/components/ui/layout/Navbar/Navbar.tsx
+++ src/components/ui/layout/Navbar/Navbar.tsx
@@ -18,19 +18,21 @@ const navlinks = [
 const Navbar: FC = () => {
 
     return (
-        <div className='wrapper py-6 flex items-center justify-between sticky bg-black/70'>
-            <div>
-                <h3 className='uppercase font-bold text-3xl'><span className='text-primaryColor'>Ride</span> Reserve</h3>
-            </div>
-            <div className='flex items-center gap-6'>
-                {
-                    navlinks.map((link, index) => (
-                        <NavLink key={index}
-                            className={({ isActive }) => isActive ? styles.active : styles.default} to={link.path}>
-                            {link.name}
-                        </NavLink>
-                    ))
-                }
+        <div className='sticky backdrop-blur-xl z-50 bg-black/60'>
+            <div className='wrapper py-6 flex items-center justify-between'>
+                <div>
+                    <h3 className='uppercase font-bold text-3xl'><span className='text-primaryColor'>Ride</span> Reserve</h3>
+                </div>
+                <div className='flex items-center gap-6'>
+                    {
+                        navlinks.map((link, index) => (
+                            <NavLink key={index}
+                                className={({ isActive }) => isActive ? styles.active : styles.default} to={link.path}>
+                                {link.name}
+                            </NavLink>
+                        ))
+                    }
+                </div>
             </div>
         </div>
     );
