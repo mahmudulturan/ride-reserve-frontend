b37f783fc4a3e2fb92834b072e9dcfdf5fca2ee1
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-08-29 11:04:32 +0600

    add: getCars endpoints on rtk query api

---
+++ src/redux/features/car/carApi.ts
@@ -0,0 +1,26 @@
+import baseApi from "@/redux/baseApi";
+
+interface ICar {
+    _id: string;
+    name: string;
+    description: string;
+    color: string;
+    isElectric: boolean;
+    status: 'available' | 'unavailable';
+    features: string[];
+    pricePerHour: number;
+    isDeleted: boolean;
+    createdAt: string;
+    updatedAt: string;
+}
+
+const carApi = baseApi.injectEndpoints({
+    endpoints: (build) => ({
+        getCars: build.query<ICar[], void>({
+            query: () => "/car",
+        })
+    })
+})
+
+
+export const { useGetCarsQuery } = carApi;
