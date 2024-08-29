315d1fe0de0d26732cb3d1d871e1d044971438a6
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-08-29 17:19:57 +0600

    add: auth slice

---
+++ src/redux/features/auth/authSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    isAuthenticate: false,
+    isAuthLoading: true,
+    user: null,
+}
+
+const usersSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        saveUser: (state, { payload }) => {
+            state.isAuthenticate = payload.isAuthenticate;
+            state.isAuthLoading = payload.isLoading;
+            state.user = payload.user;
+        },
+        removeUser: (state) => {
+            state.isAuthenticate = false;
+            state.isAuthLoading = false;
+            state.user = null;
+        }
+    }
+})
+
+export const { saveUser, removeUser } = usersSlice.actions;
+
+export default usersSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import baseApi from "./baseApi";
+import authReducer from "./features/auth/authSlice";
 export const store = configureStore({
     reducer: {
         [baseApi.reducerPath]: baseApi.reducer,
+        authSlice: authReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware)
 })
