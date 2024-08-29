af78854abcbdbcd8db8c2aa2c749272dcb847f2c
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-08-29 14:16:26 +0600

    fix: height issues on banner section

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import Banner from './components/Banner/Banner';
+import FeaturedCars from './components/FeaturedCars/FeaturedCars';
 
 const HomePage: FC = () => {
     return (
-        <div style={{ height: "calc(100vh - 84px)" }}>
+        <div style={{ minHeight: "calc(100vh - 84px)" }}>
             <Banner />
+            <FeaturedCars />
         </div>
     );
 };
