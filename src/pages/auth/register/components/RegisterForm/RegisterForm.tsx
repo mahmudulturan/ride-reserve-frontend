9f07c761ef878becbd44d880138876303cdb618a
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-08-29 16:26:30 +0600

    validate: register form with react hook form

--- src/pages/auth/register/components/RegisterForm/RegisterForm.tsx
+++ src/pages/auth/register/components/RegisterForm/RegisterForm.tsx
@@ -1,34 +1,57 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+// import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { FC } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+interface IRegisterInputs {
+    name: string
+    email: string
+    address: string
+    password: string
+    role: string
+}
 
 const RegisterForm: FC = () => {
+    // const [role, setRole] = useState("");
+    const { register, handleSubmit, formState: { errors } } = useForm<IRegisterInputs>()
 
-    const handleFormSubmit = () => {
-
+    const onSubmit: SubmitHandler<IRegisterInputs> = (data) => {
+        const reqData = {
+            name: data.name,
+            email: data.email,
+            address: data.address,
+            password: data.password,
+            role: "user"
+        }
+        console.log(reqData);
     }
+
     return (
-        <form className='max-w-xl w-full mx-auto space-y-2' onSubmit={handleFormSubmit}>
+        <form className='max-w-xl w-full mx-auto space-y-2' onSubmit={handleSubmit(onSubmit)}>
             <div className='space-y-1'>
                 <label htmlFor="name">Name</label>
-                <Input className='w-full' type="text" name='name' placeholder='Type your name...' />
+                <Input {...register('name', { required: true })} className='w-full' type="text" name='name' placeholder='Type your name...' />
+                {errors.name && <span>Name is required</span>}
             </div>
             <div className='space-y-1'>
                 <label htmlFor="email">Email</label>
-                <Input className='w-full' type="email" name='email' placeholder='Type your email...' />
+                <Input {...register('email', { required: true })} className='w-full' type="email" name='email' placeholder='Type your email...' />
+                {errors.email && <span>Email is required</span>}
             </div>
             <div className='space-y-1'>
                 <label htmlFor="address">Address</label>
-                <Input className='w-full' type="text" name='address' placeholder='Type your address...' />
+                <Input {...register('address', { required: true })} className='w-full' type="text" name='address' placeholder='Type your address...' />
+                {errors.address && <span>Address is required</span>}
             </div>
             <div className='space-y-1'>
-                <label htmlFor="passsword">Password</label>
-                <Input className='w-full' type="password" name='passsword' placeholder='Type your passsword...' />
+                <label htmlFor="password">Password</label>
+                <Input {...register('password', { required: true })} className='w-full' type="password" name='password' placeholder='Type your passsword...' />
+                {errors.password && <span>Password is required</span>}
             </div>
-            <div className='space-y-1'>
-                <label htmlFor="address">Role</label>
-                <Select>
+            {/* <div className='space-y-1'>
+                <label htmlFor="role">Role</label>
+                <Select onValueChange={(value) => setRole(value)}>
                     <SelectTrigger className="w-full">
                         <SelectValue placeholder="Select your role" />
                     </SelectTrigger>
@@ -37,7 +60,7 @@ const RegisterForm: FC = () => {
                         <SelectItem value="user">User</SelectItem>
                     </SelectContent>
                 </Select>
-            </div>
+            </div> */}
             <div className='py-3'>
 
                 <Button type='submit' className='w-full'>Register</Button>
