e2580e55afd69012fe2bb82979cd722dddba7bab
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-08-29 10:16:12 +0600

    add: basic navbar

---
+++ src/components/ui/layout/Navbar/Navbar.tsx
@@ -0,0 +1,39 @@
+import { FC } from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './navbar.module.css';
+const navlinks = [
+    {
+        name: 'Home',
+        path: '/'
+    },
+    {
+        name: 'About',
+        path: '/about'
+    },
+    {
+        name: 'Contact',
+        path: '/contact'
+    }
+];
+const Navbar: FC = () => {
+
+    return (
+        <div className='wrapper py-6 flex items-center justify-between'>
+            <div>
+                <h3 className='uppercase font-bold text-3xl'><span className='text-primaryColor'>Ride</span> Reserve</h3>
+            </div>
+            <div className='flex items-center gap-6'>
+                {
+                    navlinks.map((link, index) => (
+                        <NavLink key={index}
+                            className={({ isActive }) => isActive ? styles.active : styles.default} to={link.path}>
+                            {link.name}
+                        </NavLink>
+                    ))
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,12 @@
+import Navbar from '@/components/ui/layout/Navbar/Navbar';
 import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 
 const MainLayout: FC = () => {
     return (
-        <div className='flex flex-col min-h-screen'>
+        <div className='flex flex-col min-h-screen bg-black text-white font-outfit'>
             <div className='flex-1'>
-                <div className='text-3xl'>Navbar</div>
+                <Navbar />
                 <Outlet />
             </div>
             <div>Footer</div>
