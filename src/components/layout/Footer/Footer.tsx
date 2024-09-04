5fdf8d629a696302659c413f62d4c039cbe5ed23
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-04 14:50:52 +0600

    add: footer section

---
+++ src/components/layout/Footer/Footer.tsx
@@ -0,0 +1,72 @@
+import Logo from '@/components/shared/Logo';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { FC } from 'react';
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from 'react-icons/fa6';
+import { Link } from 'react-router-dom';
+
+const Footer: FC = () => {
+    const year = new Date().getFullYear();
+    return (
+        <div className=''>
+            <div className='wrapper py-10 '>
+                <div>
+
+                </div>
+                <div className='my-6 flex items-start justify-between'>
+                    <div className='space-y-3'>
+                        <Logo />
+                        <p className='text-sm max-w-xs font-light text-slate-200'>Rent a car imperdiet sapien porttito the bibenum ellentesue the commodo erat nesuen.</p>
+                        <div className='flex items-center gap-3 px-2 my-4'>
+                            <Button variant={"outline"} isArrowIcon={false} size={"icon"}><FaFacebookF className='size-5' /></Button>
+                            <Button variant={"outline"} isArrowIcon={false} size={"icon"}><FaXTwitter className='size-5' /></Button>
+                            <Button variant={"outline"} isArrowIcon={false} size={"icon"}><FaInstagram className='size-5' /></Button>
+                        </div>
+                    </div>
+                    <div>
+                        <h6 className='text-xl font-bold '>Quick Links</h6>
+                        <div className='text-sm font-light  space-y-1 my-3'>
+                            <Link to={"#"} className='flex items-center gap-1 text-[#ada9ad] hover:text-primaryColor'>
+                                <div className='rounded-full size-[6px] bg-primaryColor' /> <span>About</span>
+                            </Link>
+                            <Link to={"#"} className='flex items-center gap-1 text-[#ada9ad] hover:text-primaryColor'>
+                                <div className='rounded-full size-[6px] bg-primaryColor' /> <span>Cars</span>
+                            </Link>
+                            <Link to={"#"} className='flex items-center gap-1 text-[#ada9ad] hover:text-primaryColor'>
+                                <div className='rounded-full size-[6px] bg-primaryColor' /> <span>Car Types</span>
+                            </Link>
+                            <Link to={"#"} className='flex items-center gap-1 text-[#ada9ad] hover:text-primaryColor'>
+                                <div className='rounded-full size-[6px] bg-primaryColor' /> <span>FAQ</span>
+                            </Link>
+                            <Link to={"#"} className='flex items-center gap-1 text-[#ada9ad] hover:text-primaryColor'>
+                                <div className='rounded-full size-[6px] bg-primaryColor' /> <span>Contact</span>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className='space-y-3'>
+                        <h6 className='text-xl font-bold '>Subscribe</h6>
+                        <p className='text-sm max-w-xs font-light text-slate-200'>Want to be notified about our services. Just sign up and we'll send you a notification by email.</p>
+                        <div className='pt-3'>
+                            <div className='relative'>
+                                <Input className='bg-transparent border-primaryColor focus-visible:ring-transparent  focus-visible:ring-offset-0  placeholder:text-white text-white py-7 pr-[42px] rounded-full' placeholder='Enter your email' />
+                                <div className='absolute top-0 right-3 h-full w-[40px] flex items-center justify-center'>
+                                    <Button variant={"outline"} size={"icon"} className='text-xl'></Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <hr className='my-6 border-slate-800' />
+                {/* copyright section */}
+                <div>
+                    <p className='text-xs font-extralight'>@{year} <span className='text-white font-normal border-b-[1.5px] border-primaryColor'>Ride Reserve</span>. All rights reserved.</p>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,16 @@
+import Footer from '@/components/layout/Footer/Footer';
 import Navbar from '@/components/layout/Navbar/Navbar';
 import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 
 const MainLayout: FC = () => {
     return (
-        <div className='flex flex-col min-h-screen bg-black text-white font-outfit'>
+        <div className='flex flex-col min-h-screen bg-[#1B1B1B] text-white font-outfit'>
             <div className='flex-1'>
                 <Navbar />
                 <Outlet />
             </div>
-            <div>Footer</div>
+            <Footer />
         </div>
     );
 };
