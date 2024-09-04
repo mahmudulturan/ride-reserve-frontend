0bb0a625850d7bf05ae717438f7fb1e297ddb9bd
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-04 16:59:31 +0600

    add: slider on testimonials section

--- src/pages/home/components/TestimonialsSection/TestimonialsSection.tsx
+++ src/pages/home/components/TestimonialsSection/TestimonialsSection.tsx
@@ -1,18 +1,36 @@
 import SectionHeading from '@/components/shared/SectionHeading/SectionHeading';
 import { FC } from 'react';
 import TestimonialCard from './TestimonialCard';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Autoplay } from 'swiper/modules';
 
 const TestimonialsSection: FC = () => {
     return (
-        <div className='wrapper my-20'>
+        <div className='wrapper my-20 '>
             <SectionHeading subHeading='Testimonials' heading='What Our Clients Say' />
-            <div className='grid grid-cols-3 gap-6 testimonials'>
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                loop={true}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Autoplay]}
+                className="mySwiper testimonials"
+            >
                 {
-                    Array(3).fill(null).map((_, index) => (
-                        <TestimonialCard key={index} />
+                    Array(10).fill(null).map((_, index) => (
+                        <SwiperSlide key={index}>
+                            <TestimonialCard />
+                        </SwiperSlide>
                     ))
                 }
-            </div>
+            </Swiper>
         </div>
     );
 };
