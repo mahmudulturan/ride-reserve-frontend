c28085c4c55f32b37d43e92377618039cf1c4701
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-04 17:44:21 +0600

    fix: alignment on section heading component

--- src/components/shared/SectionHeading/SectionHeading.tsx
+++ src/components/shared/SectionHeading/SectionHeading.tsx
@@ -10,7 +10,7 @@ interface ISectionHeadingProps extends React.HTMLAttributes<HTMLDivElement> {
 
 const SectionHeading: FC<ISectionHeadingProps> = ({ subHeading, heading, align = "center", className, ...props }) => {
     return (
-        <div className={cn(`flex flex-col ${align === "left" ? "items-start justify-start" : align === "right" ? "items-end justify-end" : align === "center" ? "items-center justify-center" : ""}  my-6`, className)} {...props}>
+        <div className={cn(` flex flex-col ${align === "left" ? "items-start justify-start" : align === "right" ? "items-end justify-end" : align === "center" ? "items-center justify-center text-center" : ""}  my-6`, className)} {...props}>
             <div className={sectionHeadingStyles.subHeading}>
                 {subHeading}
             </div>
