cb53ee1b3194b4816bc4cf176fa44e92ac9e88ab
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-04 17:04:16 +0600

    fix: responsivness on testimonial section

--- src/pages/home/components/TestimonialsSection/TestimonialsSection.tsx
+++ src/pages/home/components/TestimonialsSection/TestimonialsSection.tsx
@@ -12,6 +12,17 @@ const TestimonialsSection: FC = () => {
             <Swiper
                 slidesPerView={3}
                 spaceBetween={30}
+                breakpoints={{
+                    0: {
+                        slidesPerView: 1,
+                    },
+                    768: {
+                        slidesPerView: 2,
+                    },
+                    1024: {
+                        slidesPerView: 3,
+                    },
+                }}
                 autoplay={{
                     delay: 2500,
                     disableOnInteraction: false,
