6ee7744129aa94f894d4116de86ad3e3820d9aed
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-06 00:13:04 +0600

    refactor: color for login and register page for light and dark mode

--- src/pages/auth/login/LoginPage.tsx
+++ src/pages/auth/login/LoginPage.tsx
@@ -5,14 +5,14 @@ import LoginForm from './components/LoginForm/LoginForm';
 const LoginPage: FC = () => {
     return (
         <div className='min-h-screen -mt-[84px] pt-[84px]' style={{ backgroundImage: "url('https://i.ibb.co/vsvGY07/login-Page.jpg)", backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
-            <div className='bg-black/55 flex items-center justify-center' style={{ minHeight: "calc(100vh - 84px)" }}>
+            <div className='bg-white/55 dark:bg-black/55 flex items-center justify-center' style={{ minHeight: "calc(100vh - 84px)" }}>
                 <div className='flex items-center justify-center wrapper' >
-                    <div className='max-w-xl w-full bg-black/25 p-8 rounded-lg'>
+                    <div className='max-w-xl w-full bg-white/65 dark:bg-black/25 p-8 rounded-lg'>
                         <div>
                             <h1 className='text-3xl text-center font-semibold uppercase'>Login</h1>
                         </div>
                         <LoginForm />
-                        <p className='text-sm text-center'>Don't have an account? <Link to="/register" className='text-primaryColor underline font-medium'>Register Here..</Link></p>
+                        <p className='text-sm text-center'>Don't have an account? <Link to="/register" className='text-primaryColorLight dark:text-primaryColor underline font-medium'>Register Here..</Link></p>
                     </div>
                 </div>
             </div>
--- src/pages/auth/register/RegisterPage.tsx
+++ src/pages/auth/register/RegisterPage.tsx
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom';
 const RegisterPage: FC = () => {
     return (
         <div className='min-h-screen -mt-[84px] pt-[84px]' style={{ backgroundImage: "url('https://i.ibb.co/vsvGY07/login-Page.jpg)", backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
-            <div className='bg-black/55 flex items-center justify-center' style={{ minHeight: "calc(100vh - 84px)" }}>
+            <div className='bg-white/55 dark:bg-black/55 flex items-center justify-center' style={{ minHeight: "calc(100vh - 84px)" }}>
                 <div className='flex items-center justify-center wrapper' >
-                    <div className='max-w-xl w-full bg-black/25 p-8 rounded-lg'>
+                    <div className='max-w-xl w-full bg-white/65 dark:bg-black/25 p-8 rounded-lg'>
                         <div>
                             <h1 className='text-3xl text-center font-semibold uppercase'>Register</h1>
                         </div>
                         <RegisterForm />
-                        <p className='text-sm text-center'>Already have an account? <Link to="/login" className='text-primaryColor underline font-medium'>Login Here..</Link></p>
+                        <p className='text-sm text-center'>Already have an account? <Link to="/login" className=' text-primaryColorLight dark:text-primaryColor underline font-medium'>Login Here..</Link></p>
                     </div>
                 </div>
             </div>
