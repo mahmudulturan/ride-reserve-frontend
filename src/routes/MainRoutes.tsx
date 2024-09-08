40ba47ff9b5601d3c7f74f0c64dba26af4a66aad
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-08 21:58:19 +0600

    refactor: folder structure for pages

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
-import HomePage from "../pages/home/HomePage";
-import LoginPage from "@/pages/auth/login/LoginPage";
-import RegisterPage from "@/pages/auth/register/RegisterPage";
-import AllCarsPage from "@/pages/all-cars/AllCarsPage";
-import CarDetailsPage from "@/pages/car-details/CarDetailsPage";
-import AboutPage from "@/pages/about/AboutPage";
 import ErrorPage from "@/pages/error/ErrorPage";
 import DashboardLayout from "@/layouts/DashboardLayout";
+import AboutPage from "@/pages/root/about/AboutPage";
+import AllCarsPage from "@/pages/root/all-cars/AllCarsPage";
+import LoginPage from "@/pages/root/auth/login/LoginPage";
+import RegisterPage from "@/pages/root/auth/register/RegisterPage";
+import CarDetailsPage from "@/pages/root/car-details/CarDetailsPage";
+import HomePage from "@/pages/root/home/HomePage";
 
 const MainRoutes = createBrowserRouter([
     {
