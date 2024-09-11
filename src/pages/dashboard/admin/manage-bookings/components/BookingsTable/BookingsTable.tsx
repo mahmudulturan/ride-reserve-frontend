43c0f0e80bc8bc0d5983e733eeb43a0f0ec970c2
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-12 01:26:09 +0600

    refactor: bookings table styles

--- src/pages/dashboard/admin/manage-bookings/components/BookingsTable/BookingsTable.tsx
+++ src/pages/dashboard/admin/manage-bookings/components/BookingsTable/BookingsTable.tsx
@@ -1,40 +1,65 @@
 import { FC } from 'react';
 import { Button } from '@/components/ui/button';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import { useGetCarsQuery } from '@/redux/features/car/carApi';
+import { useGetBookingsQuery } from '@/redux/features/booking/bookingApi';
 
 const BookingsTable: FC = () => {
-    const { data: cars } = useGetCarsQuery();
+    const { data: bookings } = useGetBookingsQuery();
 
+    const handleChangeBookingStatus = (id: string, status: "approved" | "cancelled") => {
+        console.log(id, status)
+    }
     return (
         <div className='overflow-x-auto thin-scrollbar'>
             <Table className='my-6 border dark:border-gray-600'>
                 <TableHeader>
                     <TableRow>
                         <TableHead className="w-[100px] text-center">#</TableHead>
-                        <TableHead className='min-w-[340px]'>Name</TableHead>
-                        <TableHead>Color</TableHead>
-                        <TableHead>Status</TableHead>
-                        <TableHead>Is Electric</TableHead>
-                        <TableHead className="text-center">Price</TableHead>
+                        <TableHead className='min-w-[340px]'>Car Name</TableHead>
+                        <TableHead>Customer Name</TableHead>
+                        <TableHead>Customer Email</TableHead>
+                        <TableHead className='text-center'>Start Time - End Time</TableHead>
+                        <TableHead className="text-center">Date</TableHead>
+                        <TableHead className='text-center'>Total Cost</TableHead>
                         <TableHead className="text-center min-w-[290px]">Actions</TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {cars?.data.map((car, index) => (
-                        <TableRow key={car._id}>
-                            <TableCell className="font-medium">{index + 1}</TableCell>
+                    {bookings?.data.map((booking, index) => (
+                        <TableRow key={booking._id}>
+                            <TableCell className="font-medium text-center">{index + 1}</TableCell>
                             <TableCell>
-                                {car.name}
-                                <p className="text-sm text-slate-500 dark:text-slate-300">{car.description.slice(0, 50)}...</p>
+                                {booking.car.name}
+                                <p className="text-sm text-slate-500 dark:text-slate-300">{booking.car.description.slice(0, 50)}...</p>
                             </TableCell>
-                            <TableCell>{car.color}</TableCell>
-                            <TableCell>{car.status}</TableCell>
-                            <TableCell>{car.isElectric ? 'Yes' : 'No'}</TableCell>
-                            <TableCell className="text-right">{car.pricePerHour}</TableCell>
+                            <TableCell>
+                                {booking.user.name}
+                                <p className="text-sm text-slate-500 dark:text-slate-300">{booking.user.address.slice(0, 50)}...</p>
+                            </TableCell>
+                            <TableCell>
+                                {booking.user.email}
+                            </TableCell>
+                            <TableCell className='text-center'>{booking.startTime} - {booking.endTime ? booking.endTime : 'Running'}</TableCell>
+                            <TableCell className="text-right">{booking.date}</TableCell>
+                            <TableCell className="text-center">{booking.totalCost}</TableCell>
                             <TableCell className="text-center space-x-3">
-                                <Button variant={"secondary"} isArrowIcon={false}>Edit</Button>
-                                <Button variant={"secondary"} isArrowIcon={false}>Delete</Button>
+                                <Button
+                                    className='bg-green-500 hover:bg-green-400 dark:hover:bg-green-600 dark:bg-green-700'
+                                    // disabled={isChangeStatusLoading}
+                                    onClick={() => handleChangeBookingStatus(booking._id, "approved")}
+                                    variant={"destructive"}
+                                    isArrowIcon={false}>
+                                    Approve
+                                </Button>
+
+                                <Button
+                                    // disabled={isChangeStatusLoading}
+                                    onClick={() => handleChangeBookingStatus(booking._id, "cancelled")}
+                                    variant={"destructive"}
+                                    isArrowIcon={false}>
+                                    Cancel
+                                </Button>
+
                             </TableCell>
                         </TableRow>
                     ))}
