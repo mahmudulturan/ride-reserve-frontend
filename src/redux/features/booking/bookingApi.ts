f4c81d23fbe533243397a587c9bddb8f3aac1c52
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-12 01:07:26 +0600

    add: getMyBookings and createABooking api

--- src/redux/features/booking/bookingApi.ts
+++ src/redux/features/booking/bookingApi.ts
@@ -1,10 +1,31 @@
-import baseApi from "@/redux/baseApi";
+import baseApi, { IResponse } from "@/redux/baseApi";
+import { IUser } from "../user/userApi";
+import { ICar } from "../car/carApi";
+
+interface IBooking {
+    date: string;
+    user: IUser | string;
+    car: ICar | string;
+    startTime: string;
+    endTime: string;
+    totalCost: number;
+}
 
 const boookingApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
-        getBookings: builder.query({
-            query: () => "booking/get-bookings"
+        getBookings: builder.query<IResponse<IBooking[]>, undefined>({
+            query: () => "/bookings"
         }),
+        getMyBookings: builder.query<IResponse<IBooking[]>, undefined>({
+            query: () => "/bookings/my-bookings"
+        }),
+        createABooking: builder.mutation<IResponse<IBooking>, Partial<IBooking>>({
+            query: (body) => ({
+                url: "/bookings",
+                method: "POST",
+                body
+            })
+        })
     })
 })
 
