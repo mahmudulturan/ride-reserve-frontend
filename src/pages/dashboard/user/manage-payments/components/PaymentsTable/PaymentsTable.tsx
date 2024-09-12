fa88bd650b72eea374c6badfb4e4a107e891db9e
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-13 02:42:47 +0600

    integrate payment functionalities with frontend

--- src/pages/dashboard/user/manage-payments/components/PaymentsTable/PaymentsTable.tsx
+++ src/pages/dashboard/user/manage-payments/components/PaymentsTable/PaymentsTable.tsx
@@ -2,9 +2,20 @@ import { FC } from 'react';
 import { Button } from '@/components/ui/button';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { useGetCarsQuery } from '@/redux/features/car/carApi';
+import { useCreateAPaymentMutation } from '@/redux/features/payment/paymentApi';
 
 const PaymentsTable: FC = () => {
     const { data: cars } = useGetCarsQuery();
+
+    const [createPayment, { isLoading }] = useCreateAPaymentMutation();
+
+    const handlePayment = (carId: string) => {
+        console.log(carId);
+        const paymentInfo = { amount: 1000, currency: "USD", booking: "666da4ab80f3185f260128dc", user: "66d0543c3fe73077dbeffc21" };
+        createPayment(paymentInfo).unwrap().then((res) => {
+            window.location.replace(res.data.url);
+        });
+    };
 
     return (
         <div className='overflow-x-auto thin-scrollbar'>
@@ -34,7 +45,7 @@ const PaymentsTable: FC = () => {
                             <TableCell className="text-right">{car.pricePerHour}</TableCell>
                             <TableCell className="text-center space-x-3">
                                 <Button variant={"secondary"} isArrowIcon={false}>Edit</Button>
-                                <Button variant={"secondary"} isArrowIcon={false}>Delete</Button>
+                                <Button onClick={() => handlePayment(car._id)} disabled={isLoading} variant={"secondary"} isArrowIcon={false}>Pay</Button>
                             </TableCell>
                         </TableRow>
                     ))}
---
+++ src/redux/features/payment/paymentApi.ts
@@ -0,0 +1,21 @@
+import baseApi, { IResponse } from "@/redux/baseApi";
+
+export interface IPayment {
+    amount: number
+    currency: string
+    booking: string
+    user: string
+}
+const paymentApi = baseApi.injectEndpoints({
+    endpoints: (builder) => ({
+        createAPayment: builder.mutation<IResponse<{ url: string }>, Partial<IPayment>>({
+            query: (body) => ({
+                url: "/payments",
+                method: "POST",
+                body
+            })
+        })
+    })
+})
+
+export const { useCreateAPaymentMutation } = paymentApi;
