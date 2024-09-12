319f7e700c0dcddaf8373c924d18ee14a22c6959
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-13 04:09:34 +0600

    fix: unused declaration

--- src/pages/root/all-cars/components/SearchFilterSidebar/PriceRangeSelector/PriceRangeSelector.tsx
+++ src/pages/root/all-cars/components/SearchFilterSidebar/PriceRangeSelector/PriceRangeSelector.tsx
@@ -1,11 +1,12 @@
 import { Slider } from '@/components/ui/slider';
+import { cn } from '@/lib/utils';
 import { FC, useState } from 'react';
 
 type SliderProps = React.ComponentProps<typeof Slider>
 
 const PriceRangeSelector: FC<SliderProps> = ({ className, ...props }) => {
     const [priceRange, setPriceRange] = useState([0, 50000]);
-    
+
     return (
         <div>
             <Slider
@@ -14,7 +15,11 @@ const PriceRangeSelector: FC<SliderProps> = ({ className, ...props }) => {
                 step={1000}
                 value={priceRange}
                 onValueChange={setPriceRange}
-                className="mt-2 w-full"
+                className={cn(
+                    "mt-2 w-full",
+                    className
+                )}
+                {...props}
             />
             <div className="flex justify-between mt-2">
                 <span>${priceRange[0]}</span>
