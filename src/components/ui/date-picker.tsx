b7a02698e6b227349e5a8877663f7fa4f0b8328c
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-12 14:27:49 +0600

    fix: width issues and add label on date picker

--- src/components/ui/date-picker.tsx
+++ src/components/ui/date-picker.tsx
@@ -1,17 +1,15 @@
-"use client"
-
-import * as React from "react"
 import { Popover, PopoverContent, PopoverTrigger } from "./popover";
 import { cn } from "@/lib/utils";
 import { Button } from "./button";
 import { Calendar } from "./calendar";
 import { CalendarIcon } from "lucide-react";
 import { format } from "date-fns"
+import { FC, useState } from "react";
 
 
 
-const DatePicker: React.FC = () => {
-    const [date, setDate] = React.useState<Date>()
+const DatePicker: FC<{ label: string }> = ({ label }) => {
+    const [date, setDate] = useState<Date>()
 
     return (
         <Popover>
@@ -19,13 +17,13 @@ const DatePicker: React.FC = () => {
                 <Button
                     variant={"outline"}
                     className={cn(
-                        "w-full lg:w-[280px] justify-start text-left font-normal",
+                        "w-full justify-start text-left font-normal",
                         !date && "text-muted-foreground"
                     )}
                     isArrowIcon={false}
                 >
                     <CalendarIcon className="mr-2 h-4 w-4" />
-                    {date ? format(date, "PPP") : <span>Pick a date</span>}
+                    {date ? format(date, "PPP") : <span>{label}</span>}
                 </Button>
             </PopoverTrigger>
             <PopoverContent className="w-auto p-0">
--- src/pages/root/home/components/Banner/SearchAndFilter.tsx
+++ src/pages/root/home/components/Banner/SearchAndFilter.tsx
@@ -8,17 +8,21 @@ import { MdLocationPin } from 'react-icons/md';
 const SearchAndFilter: FC = () => {
     return (
         <div className='px-10 py-6 bg-slate-100 dark:bg-slate-800 w-full rounded-md flex-col lg:flex-row lg:rounded-full flex items-center justify-between gap-4 my-6'>
-            <div className='relative w-full '>
-                <IoCar  className='absolute left-3 top-1/2 -translate-y-1/2 text-primaryColorLight dark:text-primaryColor text-2xl' />
+            <div className='relative w-full flex-1'>
+                <IoCar className='absolute left-3 top-1/2 -translate-y-1/2 text-primaryColorLight dark:text-primaryColor text-2xl' />
                 <Input className='pl-10 bg-transparent border-primaryColorLight dark:border-primaryColor focus-visible:ring-0  focus-visible:ring-offset-0 border-2 dark:placeholder:text-white dark:text-white dark:bg-transparent py-[14px] h-auto pr-[42px] rounded-full' placeholder='Car Name' />
             </div>
-            <div className='relative w-full '>
+            <div className='relative w-full flex-1'>
                 <MdLocationPin className='absolute left-3 top-1/2 -translate-y-1/2 text-primaryColorLight dark:text-primaryColor text-2xl' />
                 <Input className=' w-full pl-10 bg-transparent border-primaryColorLight dark:border-primaryColor focus-visible:ring-0  focus-visible:ring-offset-0 border-2 dark:placeholder:text-white dark:text-white dark:bg-transparent py-[14px] h-auto pr-[42px] rounded-full' placeholder='Pick-up Location' />
             </div>
-            <DatePicker />
-            <DatePicker />
-            <Button className='hover:translate-y-0 hover:bg-black hover:text-white w-full '>Search</Button>
+            <div className='flex-1'>
+                <DatePicker label="Pick-up Date" />
+            </div>
+            <div className='flex-1'>
+                <DatePicker label="Return Date" />
+            </div>
+            <Button className='hover:translate-y-0 hover:bg-black hover:text-white w-full lg:w-auto'>Search</Button>
         </div>
     );
 };
