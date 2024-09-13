80a91f685e3c6d3d3544102be3db59a91786d40f
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-13 17:21:12 +0600

    remove unnecessary imports

--- src/pages/root/booking/components/BookingForm/BookingForm.tsx
+++ src/pages/root/booking/components/BookingForm/BookingForm.tsx
@@ -1,5 +1,4 @@
 import { Button } from '@/components/ui/button';
-import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { ICar } from '@/redux/features/car/carApi';
