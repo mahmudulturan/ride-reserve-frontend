b21710f2a6931edb6250ab1b693dc007e378a20d
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-16 22:55:41 +0600

    fix: responsiveness on select features and select additional features section

--- src/pages/dashboard/admin/manage-cars/components/CreateCarModal/SelectAdditionalFeatures.tsx
+++ src/pages/dashboard/admin/manage-cars/components/CreateCarModal/SelectAdditionalFeatures.tsx
@@ -20,9 +20,9 @@ const SelectAdditionalFeatures: FC<ISelectAdditionalFeaturesProps> = ({ features
     };
 
     return (
-        <div className='h-10 flex w-full rounded-md border text-black border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primaryColor focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-primaryColor dark:ring-offset-slate-300 dark:placeholder:text-slate-400 dark:focus-visible:ring-primaryColor duration-200 overflow-hidden'>
+        <div className='flex flex-row flex-wrap gap-2 w-full rounded-md border text-black border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primaryColor focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-primaryColor dark:ring-offset-slate-300 dark:placeholder:text-slate-400 dark:focus-visible:ring-primaryColor duration-200 overflow-hidden'>
             {features?.map((feature, index) => (
-                <span key={index} className='bg-gray-200 dark:bg-gray-700 dark:text-white px-2 py-1 rounded-full text-sm flex items-center mr-2'>
+                <span key={index} className='bg-gray-200 dark:bg-gray-700 dark:text-white px-2 py-1 rounded-full text-sm flex items-center'>
                     {feature}
                     <span onClick={() => removeFeature(index)} className='ml-2 text-red-500 cursor-pointer'><RxCross2 /></span>
                 </span>
@@ -33,7 +33,7 @@ const SelectAdditionalFeatures: FC<ISelectAdditionalFeaturesProps> = ({ features
                 type="features"
                 placeholder="Car's Features"
                 id='features'
-                className="outline-none w-full flex-grow"
+                className="outline-none flex-grow"
                 onBlur={(e) => addFeature(e.target.value)}
             />
 
