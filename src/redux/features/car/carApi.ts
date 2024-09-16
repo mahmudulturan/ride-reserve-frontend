f0e22ec5156a9cf7f2bafe812b6f8f9c78392583
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-16 07:38:11 +0600

    fix: provide car tags for all endpoints and revalidate in mutations

--- src/redux/features/car/carApi.ts
+++ src/redux/features/car/carApi.ts
@@ -25,19 +25,23 @@ const carApi = baseApi.injectEndpoints({
     endpoints: (build) => ({
         getCars: build.query<IResponse<ICar[]>, void>({
             query: () => "/cars",
+            providesTags: ["Cars"]
         }),
         getACar: build.query<IResponse<ICar>, string>({
             query: (id) => `/cars/${id}`,
+            providesTags: ["Cars"]
         }),
         createCar: build.mutation<IResponse<ICar>, Partial<ICar>>({
             query: (body) => ({
                 url: "/cars",
                 method: "POST",
                 body
-            })
+            }),
+            invalidatesTags: ["Cars"]
         }),
         getHighestPricedCar: build.query<IResponse<number>, void>({
-            query: () => "/cars/highest-price-car"
+            query: () => "/cars/highest-price-car",
+            providesTags: ["Cars"]
         })
     })
 })
