0a01c85d1cf77ae9ab2e2b8b0ad322c1cafe9507
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-17 08:03:55 +0600

    add: more item on car's sidebar

--- src/pages/root/car-details/components/CarSidebar/CarSidebar.tsx
+++ src/pages/root/car-details/components/CarSidebar/CarSidebar.tsx
@@ -3,6 +3,8 @@ import { ICar } from '@/redux/features/car/carApi';
 import { FC } from 'react';
 import { BsFillPersonFill } from 'react-icons/bs';
 import { GiCarDoor } from 'react-icons/gi';
+import { IoLogoModelS } from 'react-icons/io';
+import { IoColorPalette } from 'react-icons/io5';
 import { MdOutlineElectricCar } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 
@@ -14,9 +16,23 @@ const CarSidebar: FC<{ car: ICar | undefined }> = ({ car }) => {
                 <h3 className='text-[32px] font-bold text-center flex items-center justify-center gap-2 text-white dark:text-black'>$750 <span className='text-sm font-light'>/ rent per day</span></h3>
             </div>
             <div className='rounded-[20px] rounded-t-none bg-gray-100 dark:bg-[#222222] p-[30px] space-y-8 overflow-y-auto thin-scrollbar flex justify-between flex-col'
-                style={{ height: "calc(70vh - 275px)" }}>
+                style={{ height: "calc(75vh - 275px)" }}>
                 <div>
                     <div className="space-y-3 my-4">
+                        <div className='flex items-center justify-between gap-3 text-primaryColorLight dark:text-primaryColor text-sm '>
+                            <div className="flex items-center justify-between gap-3">
+                                <IoLogoModelS />
+                                <span className='text-slate-500 dark:text-Grayish font-light'>Model</span>
+                            </div>
+                            <span className='text-slate-700 dark:text-slate-100'> {car?.model}</span>
+                        </div>
+                        <div className='flex items-center justify-between gap-3 text-primaryColorLight dark:text-primaryColor text-sm '>
+                            <div className="flex items-center justify-between gap-3">
+                                <IoColorPalette  />
+                                <span className='text-slate-500 dark:text-Grayish font-light'>Color</span>
+                            </div>
+                            <span className='text-slate-700 dark:text-slate-100'> {car?.color}</span>
+                        </div>
                         <div className='flex items-center justify-between gap-3 text-primaryColorLight dark:text-primaryColor text-sm '>
                             <div className="flex items-center justify-between gap-3">
                                 <GiCarDoor />
