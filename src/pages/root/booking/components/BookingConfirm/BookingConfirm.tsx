afd85048038e2cf0b462bb2396626fdd36fe540e
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-23 16:15:57 +0600

    feat: show all infos on booking confirm modal

--- src/pages/root/booking/components/BookingConfirm/BookingConfirm.tsx
+++ src/pages/root/booking/components/BookingConfirm/BookingConfirm.tsx
@@ -35,9 +35,81 @@ const BookingConfirm: FC<IBookingConfirmProps> = ({ reset, open, setOpen, bookin
                         Review your booking info and confirm it!
                     </DialogDescription>
                 </DialogHeader>
-                <div>
+                <div className='px-2  '>
+                    <div className='flex-1 h-[40vh] overflow-y-auto thin-scrollbar'>
+                        <div>
+                            <DialogTitle>Car Details</DialogTitle>
+                            <div className='my-1'>
+                                <div className="grid grid-cols-1 md:grid-cols-2 mb-3">
+                                    <div className='mt-2 mr-3 text-sm col-span-2'>
+                                        <span className='text-slate-500 dark:text-Grayish font-light'>Car Name : </span>
+                                        <span className='text-slate-700 dark:text-slate-100'>{bookingData?.car?.name}</span>
+                                    </div>
+                                    <div className='mt-2 mr-3 text-sm col-span-2'>
+                                        <span className='text-slate-500 dark:text-Grayish font-light'>Car Description : </span>
+                                        <span className='text-slate-700 dark:text-slate-100'>{bookingData?.car?.description}</span>
+                                    </div>
+                                    <div className='mt-2 mr-3 text-sm '>
+                                        <span className='text-slate-500 dark:text-Grayish font-light'>Car Type : </span>
+                                        <span className='text-slate-700 dark:text-slate-100'>{bookingData?.car?.carType}</span>
+                                    </div>
+                                    <div className='mt-2 mr-3 text-sm '>
+                                        <span className='text-slate-500 dark:text-Grayish font-light'>Doors : </span>
+                                        <span className='text-slate-700 dark:text-slate-100'> {bookingData?.car?.totalDoors}</span>
+                                    </div>
+                                    <div className='mt-2 mr-3 text-sm '>
+                                        <span className='text-slate-500 dark:text-Grayish font-light'>Passengers : </span>
+                                        <span className='text-slate-700 dark:text-slate-100'> {bookingData?.car?.totalPassengers}</span>
 
-                    <Button onClick={handleConfirmBooking}>Confirm</Button>
+                                    </div>
+                                    <div className='mt-2 mr-3 text-sm '>
+                                        <span className='text-slate-500 dark:text-Grayish font-light'>Electric : </span>
+                                        <span className='text-slate-700 dark:text-slate-100'>{bookingData?.car?.isElectric ? "Yes" : "No"}</span>
+                                    </div>
+                                    <div className='mt-2 mr-3 text-sm col-span-2'>
+                                        <span className='text-slate-500 dark:text-Grayish font-light'>Features : </span>
+                                        <span className='text-slate-700 dark:text-slate-100'>{bookingData?.car?.features.map((feature) => feature + ", ")}</span>
+                                    </div>
+                                    <div className='mt-2 mr-3 text-sm col-span-2'>
+                                        <span className='text-slate-500 dark:text-Grayish font-light'>Additional Features: </span>
+                                        <span className='text-slate-700 dark:text-slate-100'>{bookingData?.car?.additionalFeatures.map((feature) => feature + ", ")}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <DialogTitle>User Details</DialogTitle>
+                            <div className='my-1'>
+                                <div className="grid grid-cols-1 md:grid-cols-2 mb-3">
+                                    <div className='mt-2 mr-3 text-sm col-span-2'>
+                                        <span className='text-slate-500 dark:text-Grayish font-light'>User Name : </span>
+                                        <span className='text-slate-700 dark:text-slate-100'>{bookingData?.user?.name}</span>
+                                    </div>
+                                    <div className='mt-2 mr-3 text-sm col-span-2'>
+                                        <span className='text-slate-500 dark:text-Grayish font-light'>User Email : </span>
+                                        <span className='text-slate-700 dark:text-slate-100'>{bookingData?.user?.email}</span>
+                                    </div>
+                                    <div className='mt-2 mr-3 text-sm '>
+                                        <span className='text-slate-500 dark:text-Grayish font-light'>User NID/Passport : </span>
+                                        <span className='text-slate-700 dark:text-slate-100'>{bookingData?.nidOrPassport}</span>
+                                    </div>
+                                    <div className='mt-2 mr-3 text-sm '>
+                                        <span className='text-slate-500 dark:text-Grayish font-light'>User Driving License : </span>
+                                        <span className='text-slate-700 dark:text-slate-100'> {bookingData?.drivingLicense}</span>
+                                    </div>
+                                    <div className='mt-2 mr-3 text-sm '>
+                                        <span className='text-slate-500 dark:text-Grayish font-light'>Payment Account No. : </span>
+                                        <span className='text-slate-700 dark:text-slate-100'> {bookingData?.accountNo}</span>
+                                    </div>
+                                    <div className='mt-2 mr-3 text-sm '>
+                                        <span className='text-slate-500 dark:text-Grayish font-light'>Payment Method : </span>
+                                        <span className='text-slate-700 dark:text-slate-100'>{bookingData?.paymentMethod}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <Button onClick={handleConfirmBooking} className='w-full'>Confirm</Button>
                 </div>
             </DialogContent>
         </Dialog>
