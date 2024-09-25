e92374250e95ee7f5860f67c0d6ded5df2375864
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-25 21:34:05 +0600

    add: link tag to book now button banner

--- src/pages/root/home/components/Banner/Banner.tsx
+++ src/pages/root/home/components/Banner/Banner.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button';
 import { FC } from 'react';
 import SearchAndFilter from './SearchAndFilter';
+import { Link } from 'react-router-dom';
 
 const Banner: FC = () => {
     return (
@@ -9,7 +10,7 @@ const Banner: FC = () => {
                 <div className='flex flex-col items-center justify-center wrapper text-center gap-2' style={{ minHeight: "calc(100vh - 84px)" }}>
                     <h1 className='text-4xl md:text-7xl font-bold max-w-2xl'>Discover the Luxury of Car Rental</h1>
                     <p className='font-light dark:text-gray-300 text-gray-900 mb-4 max-w-sm'>Experience the thrill of driving the finest cars on the road. Book your rental today.</p>
-                    <Button>Book Now</Button>
+                    <Link to={'/cars'}><Button>Book Now</Button></Link>
                     <SearchAndFilter />
                 </div>
             </div>
