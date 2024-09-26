2169bbd8445aa8cdca187097026da2572b9e032c
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-26 12:57:36 +0600

    refactor: erropage for others error

--- src/pages/error/ErrorPage.tsx
+++ src/pages/error/ErrorPage.tsx
@@ -1,19 +1,35 @@
 import { FC } from 'react';
 import errorImage from '../../assets/images/not_found.svg'
+import serverErrorImage from '../../assets/images/serverErrorImage.svg';
 import { Button } from '@/components/ui/button';
-import { Link } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
 const ErrorPage: FC = () => {
+    const routeError: any = useRouteError();
+    console.log(routeError.status);
     return (
         <div className='min-h-screen flex items-center justify-center wrapper'>
-            <div>
-                <h1 className='text-3xl font-bold text-center my-6'>Page Not Found</h1>
-                <img src={errorImage} alt="" />
-                <div className='text-center my-6'>
-                    <Link to={'/'}>
-                        <Button variant={"secondary"}>Back To Home</Button>
-                    </Link>
-                </div>
-            </div>
+            {
+                routeError.status === 404 ?
+                    <div>
+                        <h1 className='text-3xl font-bold text-center my-6'>Page Not Found!</h1>
+                        <img src={errorImage} className='' alt="" />
+                        <div className='text-center my-6'>
+                            <Link to={'/'}>
+                                <Button variant={"secondary"}>Back To Home</Button>
+                            </Link>
+                        </div>
+                    </div>
+                    :
+                    <div>
+                        <h1 className='text-3xl font-bold text-center my-6'>Server Error!</h1>
+                        <img src={serverErrorImage} className='' alt="" />
+                        <div className='text-center my-6'>
+                            <Link to={'/'}>
+                                <Button variant={"secondary"}>Back To Home</Button>
+                            </Link>
+                        </div>
+                    </div>
+            }
         </div>
     );
 };
