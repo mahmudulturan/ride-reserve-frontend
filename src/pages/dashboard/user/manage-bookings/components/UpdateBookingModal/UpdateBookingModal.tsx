0ed2d67e6c1db779b2f40a1627e152a43d4d1184
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-26 19:27:40 +0600

    add: validation for update booking form

--- src/pages/dashboard/user/manage-bookings/components/UpdateBookingModal/UpdateBookingModal.tsx
+++ src/pages/dashboard/user/manage-bookings/components/UpdateBookingModal/UpdateBookingModal.tsx
@@ -13,15 +13,27 @@ import { Label } from '@/components/ui/label';
 const UpdateBookingModal: FC<{ booking: IBooking }> = ({ booking }) => {
     const [open, setOpen] = useState<boolean>(false);
     const user = useAppSelector(state => state.authSlice.user);
-    const [paymentMethod, setPaymentMethod] = useState<string>("");
+    const [paymentMethod, setPaymentMethod] = useState<string>(booking.paymentMethod);
     const [paymentMethodError, setPaymentMethodError] = useState<boolean>(false);
 
-    const { register, handleSubmit, reset, formState: { errors }, } = useForm<Partial<IBooking>>();
+    const { register, handleSubmit, formState: { errors }, } = useForm<Partial<IBooking>>();
 
     const onSubmit: SubmitHandler<Partial<IBooking>> = (data) => {
-        console.log(booking)
-        console.log(data);
-        reset();
+        if (!paymentMethod) {
+            return setPaymentMethodError(true);
+        } else {
+            setPaymentMethodError(false);
+        }
+        if (!user || !paymentMethod) return;
+        const reqData = {
+            nidOrPassport: data.nidOrPassport || '',
+            drivingLicense: data.drivingLicense || '',
+            paymentMethod: paymentMethod,
+            accountNo: data.accountNo || ''
+        }
+
+        console.log(reqData);
+        // reset();
     }
 
     return (
@@ -86,7 +98,7 @@ const UpdateBookingModal: FC<{ booking: IBooking }> = ({ booking }) => {
                                     defaultValue={booking.nidOrPassport}
                                     className="w-full"
                                 />
-                                {errors.nidOrPassport && <span className='text-red-400 text-sm px-3'>Name is required</span>}
+                                {errors.nidOrPassport && <span className='text-red-400 text-sm px-3'>NID/Passport is required</span>}
                             </div>
                             <div className="space-y-2">
                                 <Label htmlFor="drivingLicense" className="text-right">
@@ -99,7 +111,7 @@ const UpdateBookingModal: FC<{ booking: IBooking }> = ({ booking }) => {
                                     placeholder='Your Driving Licencese No.'
                                     className="w-full"
                                 />
-                                {errors.drivingLicense && <span className='text-red-400 text-sm px-3'>Name is required</span>}
+                                {errors.drivingLicense && <span className='text-red-400 text-sm px-3'>Driving License is required</span>}
                             </div>
                             <div className="space-y-2 col-span-2">
                                 <Label htmlFor="accountNo" className="text-right">
