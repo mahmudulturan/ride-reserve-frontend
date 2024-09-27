426dd358c4d14b29bbb73a8f82ddaa261bd65da3
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-27 10:21:47 +0600

    chore: hide additional features section while there's no additional features

--- src/pages/root/car-details/components/CarFeatures/CarFeatures.tsx
+++ src/pages/root/car-details/components/CarFeatures/CarFeatures.tsx
@@ -22,19 +22,22 @@ const CarFeatures: FC<ICarFeaturesProps> = ({ features, additionalFeatures }) =>
 
                 </div>
             </div>
-            <div className='space-y-3'>
-                <h3 className='text-[21px] font-bold'>Additional Features</h3>
-                <div className='space-y-3 my-4'>
-                    {
-                        additionalFeatures.map((feature, index) => (
-                            <div key={index} className='flex items-center gap-3'>
-                                <div className='p-[10px] bg-gray-100 dark:bg-[#222222] rounded-full text-xl text-primaryColor'><MdOutlineDoubleArrow /></div>
-                                <span className='dark:text-Grayish text-gray-500 text-sm font-light'>{feature}</span>
-                            </div>))
-                    }
+            {
+                additionalFeatures.length > 0 &&
+                <div className='space-y-3'>
+                    <h3 className='text-[21px] font-bold'>Additional Features</h3>
+                    <div className='space-y-3 my-4'>
+                        {
+                            additionalFeatures.map((feature, index) => (
+                                <div key={index} className='flex items-center gap-3'>
+                                    <div className='p-[10px] bg-gray-100 dark:bg-[#222222] rounded-full text-xl text-primaryColor'><MdOutlineDoubleArrow /></div>
+                                    <span className='dark:text-Grayish text-gray-500 text-sm font-light'>{feature}</span>
+                                </div>))
+                        }
 
+                    </div>
                 </div>
-            </div>
+            }
         </div>
     );
 };
