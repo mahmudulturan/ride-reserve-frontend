13d40b6e57e805f35ccc16dd1c98ae5708431669
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-29 21:51:45 +0600

    add: loading animation on loader component

--- src/components/shared/Loader/Loader.tsx
+++ src/components/shared/Loader/Loader.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
+import Lottie from "lottie-react";
+import loadingAnimations from '../../../assets/animations/loadingAnimation.json';
 
 const Loader: FC = () => {
     return (
-        <div>
-            Loader
+        <div className='flex items-center justify-center min-h-[80vh]'>
+            <Lottie animationData={loadingAnimations} className='size-[300px]' loop={true} />
         </div>
     );
 };
