965a03dd72067a24ddd7840a2f96022fc2fb9e88
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-29 16:32:17 +0600

    add: pagination controlls on user bookings table

--- src/pages/dashboard/user/manage-bookings/components/BookingsTable/UserBookingsTable.tsx
+++ src/pages/dashboard/user/manage-bookings/components/BookingsTable/UserBookingsTable.tsx
@@ -4,9 +4,12 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@
 import { useCancelBookingMutation, useGetMyBookingsQuery } from '@/redux/features/booking/bookingApi';
 import UpdateBookingModal from '../UpdateBookingModal/UpdateBookingModal';
 import { toast } from '@/components/ui/use-toast';
+import { useSearchParams } from 'react-router-dom';
+import PaginationControlls from '@/components/shared/PaginationControlls/PaginationControlls';
 
 const UserBookingsTable: FC = () => {
-    const { data: bookings } = useGetMyBookingsQuery({});
+    const [searchParams, setSearchParams] = useSearchParams();
+    const { data: bookings } = useGetMyBookingsQuery({ page: Number(searchParams.get('page')) || 1 });
 
     const [cancelBooking, { isLoading }] = useCancelBookingMutation();
 
@@ -32,60 +35,70 @@ const UserBookingsTable: FC = () => {
     }
     return (
         <div className='overflow-x-auto thin-scrollbar'>
-            <Table className='my-6 border dark:border-gray-600'>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead className="w-[100px] text-center">#</TableHead>
-                        <TableHead className='min-w-[340px]'>Car Name</TableHead>
-                        <TableHead className='text-center min-w-[190px]'>Start Time - End Time</TableHead>
-                        <TableHead className="text-center">Date</TableHead>
-                        <TableHead className="text-center min-w-[90px]">Payment Info</TableHead>
-                        <TableHead className="text-center">Driving</TableHead>
-                        <TableHead className="text-center">NID/Passport</TableHead>
-                        <TableHead className='text-center min-w-[100px]'>Total Cost</TableHead>
-                        <TableHead className='text-center'>Status</TableHead>
-                        <TableHead className="text-center min-w-[290px]">Actions</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {bookings?.data.map((booking, index) => (
-                        <TableRow key={booking._id}>
-                            <TableCell className="font-medium text-center">{index + 1}</TableCell>
-                            <TableCell>
-                                {booking.car.name}
-                                <p className="text-sm text-slate-500 dark:text-slate-300">{booking.car.description.slice(0, 30)}...</p>
-                            </TableCell>
-                            <TableCell className='text-center'>{booking.startTime} - {booking.endTime ? booking.endTime : 'Running'}</TableCell>
-                            <TableCell className="text-center">{booking.date}</TableCell>
-                            <TableCell className="">
-                                {booking.accountNo}
-                                <p className="text-sm text-slate-500 dark:text-slate-300">{booking.paymentMethod}</p>
-                            </TableCell>
-                            <TableCell className="text-center">{booking.drivingLicense}</TableCell>
-                            <TableCell className="text-center">{booking.nidOrPassport}</TableCell>
-                            <TableCell className="text-center">{booking.totalCost}</TableCell>
-                            <TableCell className="text-center">
-                                {booking.status === "pending" && "Pending"}
-                                {booking.status === "cancelled" && "Cancelled"}
-                                {booking.status === "completed" && "Completed"}
-                                {booking.status === "approved" && "Approved"}
-                            </TableCell>
-                            <TableCell className="text-center space-x-3">
-                                <UpdateBookingModal booking={booking} />
+            {
+                bookings ?
+                    <>
+                        <Table className='my-6 border dark:border-gray-600'>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[100px] text-center">#</TableHead>
+                                    <TableHead className='min-w-[340px]'>Car Name</TableHead>
+                                    <TableHead className='text-center min-w-[190px]'>Start Time - End Time</TableHead>
+                                    <TableHead className="text-center">Date</TableHead>
+                                    <TableHead className="text-center min-w-[90px]">Payment Info</TableHead>
+                                    <TableHead className="text-center">Driving</TableHead>
+                                    <TableHead className="text-center">NID/Passport</TableHead>
+                                    <TableHead className='text-center min-w-[100px]'>Total Cost</TableHead>
+                                    <TableHead className='text-center'>Status</TableHead>
+                                    <TableHead className="text-center min-w-[290px]">Actions</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {bookings?.data.bookings.map((booking, index) => (
+                                    <TableRow key={booking._id}>
+                                        <TableCell className="font-medium text-center">{index + 1}</TableCell>
+                                        <TableCell>
+                                            {booking.car.name}
+                                            <p className="text-sm text-slate-500 dark:text-slate-300">{booking.car.description.slice(0, 30)}...</p>
+                                        </TableCell>
+                                        <TableCell className='text-center'>{booking.startTime} - {booking.endTime ? booking.endTime : 'Running'}</TableCell>
+                                        <TableCell className="text-center">{booking.date}</TableCell>
+                                        <TableCell className="">
+                                            {booking.accountNo}
+                                            <p className="text-sm text-slate-500 dark:text-slate-300">{booking.paymentMethod}</p>
+                                        </TableCell>
+                                        <TableCell className="text-center">{booking.drivingLicense}</TableCell>
+                                        <TableCell className="text-center">{booking.nidOrPassport}</TableCell>
+                                        <TableCell className="text-center">{booking.totalCost}</TableCell>
+                                        <TableCell className="text-center">
+                                            {booking.status === "pending" && "Pending"}
+                                            {booking.status === "cancelled" && "Cancelled"}
+                                            {booking.status === "completed" && "Completed"}
+                                            {booking.status === "approved" && "Approved"}
+                                        </TableCell>
+                                        <TableCell className="text-center space-x-3">
+                                            <UpdateBookingModal booking={booking} />
 
-                                <Button
-                                    disabled={isLoading || booking.status !== "pending"}
-                                    onClick={() => handleCancelBooking(booking._id)}
-                                    variant={"destructive"}
-                                    isArrowIcon={false}>
-                                    {booking.status === "cancelled" ? "Cancelled" : "Cancel"}
-                                </Button>
+                                            <Button
+                                                disabled={isLoading || booking.status !== "pending"}
+                                                onClick={() => handleCancelBooking(booking._id)}
+                                                variant={"destructive"}
+                                                isArrowIcon={false}>
+                                                {booking.status === "cancelled" ? "Cancelled" : "Cancel"}
+                                            </Button>
 
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                        <PaginationControlls itemsCount={bookings.data.bookingsCount} searchParams={searchParams} setSearchParams={setSearchParams} />
+                    </>
+                    :
+                    <div className='min-h-[70vh] w-full flex justify-center items-center'>
+                        <h3 className='text-2xl md:text-4xl font-bold'>No bookings found!</h3>
+                    </div>
+            }
         </div>
     );
 };
--- src/pages/dashboard/user/manage-payments/components/PaymentsTable/PaymentsTable.tsx
+++ src/pages/dashboard/user/manage-payments/components/PaymentsTable/PaymentsTable.tsx
@@ -4,9 +4,12 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@
 import { useCreateAPaymentMutation } from '@/redux/features/payment/paymentApi';
 import { useGetMyBookingsQuery } from '@/redux/features/booking/bookingApi';
 import { useAppSelector } from '@/redux/hook';
+import { useSearchParams } from 'react-router-dom';
+import PaginationControlls from '@/components/shared/PaginationControlls/PaginationControlls';
 
 const PaymentsTable: FC = () => {
-    const { data: bookings } = useGetMyBookingsQuery({ status: "completed" });
+    const [searchParams, setSearchParams] = useSearchParams();
+    const { data: bookings } = useGetMyBookingsQuery({ status: "completed", page: Number(searchParams.get("page")) || 1 });
 
     const [createPayment, { isLoading }] = useCreateAPaymentMutation();
     const user = useAppSelector(state => state.authSlice.user);
@@ -20,54 +23,64 @@ const PaymentsTable: FC = () => {
 
     return (
         <div className='overflow-x-auto thin-scrollbar'>
-            <Table className='my-6 border dark:border-gray-600'>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead className="w-[100px] text-center">#</TableHead>
-                        <TableHead className='min-w-[240px]'>Car Name</TableHead>
-                        <TableHead className='text-center min-w-[190px]'>Start Time - End Time</TableHead>
-                        <TableHead className="text-center  min-w-[100px]">Date</TableHead>
-                        <TableHead className="text-center">Driving</TableHead>
-                        <TableHead className="text-center">NID/Passport</TableHead>
-                        <TableHead className='text-center min-w-[100px]'>Total Cost</TableHead>
-                        <TableHead className="text-center min-w-[120px]">Payment Info</TableHead>
-                        <TableHead className='text-center'>Status</TableHead>
-                        <TableHead className="text-center min-w-[290px]">Actions</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {bookings?.data.map((booking, index) => (
-                        <TableRow key={booking._id}>
-                            <TableCell className="font-medium text-center">{index + 1}</TableCell>
-                            <TableCell>
-                                {booking.car.name}
-                                <p className="text-sm text-slate-500 dark:text-slate-300">{booking.car.description.slice(0, 30)}...</p>
-                            </TableCell>
-                            <TableCell className='text-center'>{booking.startTime} - {booking.endTime ? booking.endTime : 'Running'}</TableCell>
-                            <TableCell className="text-center">{booking.date}</TableCell>
-                            <TableCell className="text-center">{booking.drivingLicense}</TableCell>
-                            <TableCell className="text-center">{booking.nidOrPassport}</TableCell>
-                            <TableCell className="text-center">{booking.totalCost}</TableCell>
-                            <TableCell className="">
-                                {booking.accountNo}
-                                <p className="text-sm text-slate-500 dark:text-slate-300">{booking.paymentMethod}</p>
-                            </TableCell>
-                            <TableCell className="text-center">
-                                {booking.paymentStatus === "unpaid" && "Unpaid"}
-                                {booking.paymentStatus === "paid" && "Paid"}
-                            </TableCell>
-                            <TableCell className="text-center space-x-3">
-                                <Button
-                                    onClick={() => handlePayment(booking._id, booking.totalCost)}
-                                    disabled={isLoading || booking.paymentStatus === "paid"}
-                                    className="btn-primary btn-sm">
-                                    Pay Now
-                                </Button>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+            {
+                bookings ?
+                    <>
+                        <Table className='my-6 border dark:border-gray-600'>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[100px] text-center">#</TableHead>
+                                    <TableHead className='min-w-[240px]'>Car Name</TableHead>
+                                    <TableHead className='text-center min-w-[190px]'>Start Time - End Time</TableHead>
+                                    <TableHead className="text-center  min-w-[100px]">Date</TableHead>
+                                    <TableHead className="text-center">Driving</TableHead>
+                                    <TableHead className="text-center">NID/Passport</TableHead>
+                                    <TableHead className='text-center min-w-[100px]'>Total Cost</TableHead>
+                                    <TableHead className="text-center min-w-[120px]">Payment Info</TableHead>
+                                    <TableHead className='text-center'>Status</TableHead>
+                                    <TableHead className="text-center min-w-[290px]">Actions</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {bookings?.data.bookings.map((booking, index) => (
+                                    <TableRow key={booking._id}>
+                                        <TableCell className="font-medium text-center">{index + 1}</TableCell>
+                                        <TableCell>
+                                            {booking.car.name}
+                                            <p className="text-sm text-slate-500 dark:text-slate-300">{booking.car.description.slice(0, 30)}...</p>
+                                        </TableCell>
+                                        <TableCell className='text-center'>{booking.startTime} - {booking.endTime ? booking.endTime : 'Running'}</TableCell>
+                                        <TableCell className="text-center">{booking.date}</TableCell>
+                                        <TableCell className="text-center">{booking.drivingLicense}</TableCell>
+                                        <TableCell className="text-center">{booking.nidOrPassport}</TableCell>
+                                        <TableCell className="text-center">{booking.totalCost}</TableCell>
+                                        <TableCell className="">
+                                            {booking.accountNo}
+                                            <p className="text-sm text-slate-500 dark:text-slate-300">{booking.paymentMethod}</p>
+                                        </TableCell>
+                                        <TableCell className="text-center">
+                                            {booking.paymentStatus === "unpaid" && "Unpaid"}
+                                            {booking.paymentStatus === "paid" && "Paid"}
+                                        </TableCell>
+                                        <TableCell className="text-center space-x-3">
+                                            <Button
+                                                onClick={() => handlePayment(booking._id, booking.totalCost)}
+                                                disabled={isLoading || booking.paymentStatus === "paid"}
+                                                className="btn-primary btn-sm">
+                                                Pay Now
+                                            </Button>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                        <PaginationControlls searchParams={searchParams} setSearchParams={setSearchParams} itemsCount={bookings?.data.bookingsCount} />
+                    </>
+                    :
+                    <div className='min-h-[70vh] w-full flex justify-center items-center'>
+                        <h3 className='text-2xl md:text-4xl font-bold'>No payments found!</h3>
+                    </div>
+            }
         </div>
     );
 };
