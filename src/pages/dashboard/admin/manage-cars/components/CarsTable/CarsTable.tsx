9ac6dca0607881f00f94d137561af3aafade1b72
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-29 16:18:04 +0600

    add: pagination controlls on cars table

--- src/pages/dashboard/admin/manage-cars/components/CarsTable/CarsTable.tsx
+++ src/pages/dashboard/admin/manage-cars/components/CarsTable/CarsTable.tsx
@@ -5,9 +5,12 @@ import { toast } from '@/components/ui/use-toast';
 import { useDeleteCarMutation, useGetCarsQuery } from '@/redux/features/car/carApi';
 import { FC } from 'react';
 import EditCarModal from '../EditCarModal/EditCarModal';
+import { useSearchParams } from 'react-router-dom';
+import PaginationControlls from '@/components/shared/PaginationControlls/PaginationControlls';
 
 const CarsTable: FC = () => {
-    const { data: cars, isLoading } = useGetCarsQuery({});
+    const [searchParams, setSearchParams] = useSearchParams();
+    const { data: cars, isLoading } = useGetCarsQuery({ page: searchParams.get("page") || "1" });
 
     const [deleteCar, { isLoading: isDeleting }] = useDeleteCarMutation();
 
@@ -40,49 +43,52 @@ const CarsTable: FC = () => {
         <div className='overflow-x-auto thin-scrollbar'>
             {
                 cars ?
-                    <Table className='my-6 border dark:border-gray-600'>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead className="w-[100px] text-center">#</TableHead>
-                                <TableHead className='min-w-[340px]'>Name</TableHead>
-                                <TableHead className='text-center'>Type</TableHead>
-                                <TableHead className='text-center'>Model</TableHead>
-                                <TableHead className='text-center'>Color</TableHead>
-                                <TableHead className='text-center'>Status</TableHead>
-                                <TableHead className="text-center">Is Electric</TableHead>
-                                <TableHead className="text-center">Price</TableHead>
-                                <TableHead className="text-center min-w-[290px]">Actions</TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {cars?.data?.cars?.map((car, index) => (
-                                <TableRow key={car._id}>
-                                    <TableCell className="font-medium text-center">{index + 1}</TableCell>
-                                    <TableCell>
-                                        {car.name}
-                                        <p className="text-sm text-slate-500 dark:text-slate-300">{car.description.slice(0, 50)}...</p>
-                                    </TableCell>
-                                    <TableCell className='text-center'>{car.carType}</TableCell>
-                                    <TableCell className='text-center'>{car.model}</TableCell>
-                                    <TableCell className='text-center'>{car.color}</TableCell>
-                                    <TableCell className='text-center'>{car.status}</TableCell>
-                                    <TableCell className='text-center'>{car.isElectric ? 'Yes' : 'No'}</TableCell>
-                                    <TableCell className="text-center">{car.pricePerHour}</TableCell>
-                                    <TableCell className="text-center space-x-3">
-                                        <EditCarModal car={car} />
-                                        <Button
-                                            onClick={() => handleCarDelete(car._id)}
-                                            variant={"secondary"}
-                                            isArrowIcon={false}
-                                            disabled={isDeleting}
-                                        >
-                                            Delete
-                                        </Button>
-                                    </TableCell>
+                    <>
+                        <Table className='my-6 border dark:border-gray-600'>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[100px] text-center">#</TableHead>
+                                    <TableHead className='min-w-[340px]'>Name</TableHead>
+                                    <TableHead className='text-center'>Type</TableHead>
+                                    <TableHead className='text-center'>Model</TableHead>
+                                    <TableHead className='text-center'>Color</TableHead>
+                                    <TableHead className='text-center'>Status</TableHead>
+                                    <TableHead className="text-center">Is Electric</TableHead>
+                                    <TableHead className="text-center">Price</TableHead>
+                                    <TableHead className="text-center min-w-[290px]">Actions</TableHead>
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
+                            </TableHeader>
+                            <TableBody>
+                                {cars?.data?.cars?.map((car, index) => (
+                                    <TableRow key={car._id}>
+                                        <TableCell className="font-medium text-center">{((Number(searchParams.get("page")) || 1) * 8 - 8) + (index + 1)}</TableCell>
+                                        <TableCell>
+                                            {car.name}
+                                            <p className="text-sm text-slate-500 dark:text-slate-300">{car.description.slice(0, 50)}...</p>
+                                        </TableCell>
+                                        <TableCell className='text-center'>{car.carType}</TableCell>
+                                        <TableCell className='text-center'>{car.model}</TableCell>
+                                        <TableCell className='text-center'>{car.color}</TableCell>
+                                        <TableCell className='text-center'>{car.status}</TableCell>
+                                        <TableCell className='text-center'>{car.isElectric ? 'Yes' : 'No'}</TableCell>
+                                        <TableCell className="text-center">{car.pricePerHour}</TableCell>
+                                        <TableCell className="text-center space-x-3">
+                                            <EditCarModal car={car} />
+                                            <Button
+                                                onClick={() => handleCarDelete(car._id)}
+                                                variant={"secondary"}
+                                                isArrowIcon={false}
+                                                disabled={isDeleting}
+                                            >
+                                                Delete
+                                            </Button>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                        <PaginationControlls searchParams={searchParams} setSearchParams={setSearchParams} itemsCount={cars?.data?.carsCount} />
+                    </>
                     :
                     <div className='min-h-[70vh] w-full flex justify-center items-center'>
                         <h3 className='text-2xl md:text-4xl font-bold'>No cars found!</h3>
