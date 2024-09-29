a064892a9e9c4219b395816ce72c399070aecc92
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-29 08:39:26 +0600

    chore: add dynamic car price on car's sidebar

--- src/pages/root/car-details/components/CarSidebar/CarSidebar.tsx
+++ src/pages/root/car-details/components/CarSidebar/CarSidebar.tsx
@@ -13,7 +13,7 @@ const CarSidebar: FC<{ car: ICar | undefined }> = ({ car }) => {
         <div className='sticky top-[128px] -mt-[128px]  rounded-[20px]' >
             <div className='rounded-[20px] rounded-b-none overflow-hidden bg-primaryColorLight dark:bg-primaryColor p-[30px]'>
 
-                <h3 className='text-[32px] font-bold text-center flex items-center justify-center gap-2 text-white dark:text-black'>$750 <span className='text-sm font-light'>/ rent per day</span></h3>
+                <h3 className='text-[32px] font-bold text-center flex items-center justify-center gap-2 text-white dark:text-black'>${car?.pricePerHour} <span className='text-sm font-light'>/ rent per day</span></h3>
             </div>
             <div className='rounded-[20px] rounded-t-none bg-gray-100 dark:bg-[#222222] p-[30px] space-y-8 overflow-y-auto thin-scrollbar flex justify-between flex-col'
                 style={{ height: "calc(75vh - 275px)" }}>
