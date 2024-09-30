51f7343e5cdf2ec36385673de30341fa64c62dc2
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-30 13:55:15 +0600

    logout user if token role and user role does'nt match

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,8 +1,9 @@
 import { useGetUserQuery } from '@/redux/features/auth/authApi';
 import { removeUser, saveUser } from '@/redux/features/auth/authSlice';
 import { useAppDispatch } from '@/redux/hook';
-import { getStoredToken } from '@/utils/tokenStorage';
+import { clearToken, getStoredToken } from '@/utils/tokenStorage';
 import { FC, useEffect } from 'react';
+import { jwtDecode } from "jwt-decode";
 
 const AuthProvider: FC<{ children: React.ReactNode }> = ({ children }) => {
     const { data: userData, isLoading: isUserLoading } = useGetUserQuery(undefined);
@@ -14,8 +15,17 @@ const AuthProvider: FC<{ children: React.ReactNode }> = ({ children }) => {
 
     useEffect(() => {
         const payloadObj = { isAuthenticate: false, user: null, isLoading: false }
+
         // if the success is true then authenticate the user
         if (userData?.success) {
+            const { role }: any = jwtDecode(token || '');
+            if (role != userData.role) {
+                clearToken();
+                payloadObj.isAuthenticate = false;
+                payloadObj.user = null
+                payloadObj.isLoading = isUserLoading;
+                return;
+            }
             payloadObj.isAuthenticate = true;
             payloadObj.user = userData?.data
             payloadObj.isLoading = isUserLoading;
